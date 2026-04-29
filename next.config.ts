f4e2af919470ae5bc616bfb76daae1dbc6d12b4e
王锋 <wf1095870395@gmail.com>
2026-04-29 23:24:12 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    allowedDevOrigins: ['172.26.92.186', '127.0.0.1', '0.0.0.0', '142.171.65.186']
 };
 
 export default nextConfig;
