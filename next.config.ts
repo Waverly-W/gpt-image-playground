ff3e30cfbed147ce41be571ebd16cfc1b727d178
王锋 <wf1095870395@gmail.com>
2026-04-30 13:16:08 +0800

    wsl

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
-    allowedDevOrigins: ['172.26.92.186', '127.0.0.1', '0.0.0.0', '142.171.65.186']
+    allowedDevOrigins: ['wsl','172.26.92.186', '127.0.0.1', '0.0.0.0', '142.171.65.186']
 };
 
 export default nextConfig;
